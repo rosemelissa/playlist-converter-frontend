38c1df75fdecef66b981f69f9bf6af3c136e9675
Melissa Rose <123melissa321@gmail.com>
2022-08-31 15:03:02 +0100

    Dont POST tracks to playlist of there are no valid ones

--- src/utils/addTracksToPlaylist.ts
+++ src/utils/addTracksToPlaylist.ts
@@ -8,19 +8,21 @@ async function addTracksToPlaylist(tracks: IYoutubeAndSpotify[], playlistID: str
             uriArray.push(track.spotify.uri);
         }
     }
-    
-    const access_token = localStorage.getItem('access_token')
-    const headers = {
-    headers: {
-        "Authorization": `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+    if (uriArray.length > 0) {
+        const access_token = localStorage.getItem('access_token')
+        const headers = {
+        headers: {
+            "Authorization": `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+        }
+        };
+        const data = {
+            "uris": uriArray,
+        }
+        const response = await axios.post(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, data, headers)
+        console.log('posting tracks to playlist' + response)        
     }
-    };
-    const data = {
-        "uris": uriArray,
-    }
-    const response = await axios.post(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, data, headers)
-    console.log('posting tracks to playlist' + response)
+
 }
 
 export default addTracksToPlaylist;
